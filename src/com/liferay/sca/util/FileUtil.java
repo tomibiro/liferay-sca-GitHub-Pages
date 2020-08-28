09612b733b797fc3bc06789051e957500a6c7193
Samuel Kong <samuel.kong@liferay.com>
2020-08-28 12:40:57 +0800

    #1 Util for working with files

---
+++ src/com/liferay/sca/util/FileUtil.java
@@ -0,0 +1,64 @@
+package com.liferay.sca.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class FileUtil {
+
+	public static String read(File file) throws IOException {
+		return read(file.getAbsolutePath());
+	}
+
+	public static String read(String filename) throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		Reader reader = null;
+
+		try {
+			reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(filename), UTF8));
+
+			int length = 0;
+			char[] buffer = new char[4096];
+
+			while ((length = reader.read(buffer)) != -1) {
+				sb.append(buffer, 0, length);
+			}
+		}
+		finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static void write(String content, File file)
+		throws IOException {
+
+		write(content, file.getAbsolutePath());
+	}
+
+	public static void write(String content, String filename)
+		throws IOException {
+
+		Writer out = new PrintWriter(filename, UTF8);
+
+		try {
+			out.write(content);
+		}
+		finally {
+			out.close();
+		}
+	}
+
+	protected static String UTF8 = "UTF-8";
+
+}
