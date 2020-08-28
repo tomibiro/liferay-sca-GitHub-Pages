6ce42e16f43ff700f51d8d3e53c181b8b9cccf16
Samuel Kong <samuel.kong@liferay.com>
2020-08-28 10:08:38 +0800

    #1 config.properties

---
+++ src/com/liferay/sca/util/PropsKeys.java
@@ -0,0 +1,15 @@
+package com.liferay.sca.util;
+
+public interface PropsKeys {
+
+	public static final String DEPENDABOT_PACKAGE_DIR = "dependabot.package.dir";
+
+	public static final String DEPENDENCIES_REPORT = "dependencies.report";
+
+	public static final String PACKAGE_FILENAMES = "package.filenames";
+
+	public static final String PROJECTS = "projects";
+	
+	public static final String SRC_CODE = "src.code";
+
+}
---
+++ src/com/liferay/sca/util/PropsValues.java
@@ -0,0 +1,11 @@
+package com.liferay.sca.util;
+
+public interface PropsValues {
+
+	public static final String[] PACKAGE_FILENAMES = PropsUtil.getArray(
+		PropsKeys.PACKAGE_FILENAMES);
+
+	public static final String[] PROJECTS = PropsUtil.getArray(
+		PropsKeys.PROJECTS);
+
+}
