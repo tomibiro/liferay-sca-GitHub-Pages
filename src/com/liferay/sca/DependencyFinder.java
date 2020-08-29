865e40704365201a4492b6576971b946594a7054
Samuel Kong <samuel.kong@liferay.com>
2020-08-29 21:08:30 +0800

    #1 Remove unused save() method

--- src/com/liferay/sca/DependencyFinder.java
+++ src/com/liferay/sca/DependencyFinder.java
@@ -1,16 +1,13 @@
 package com.liferay.sca;
 
-import com.liferay.sca.model.Dependency;
 import com.liferay.sca.model.Package;
 import com.liferay.sca.model.Report;
-import com.liferay.sca.util.FileUtil;
 import com.liferay.sca.util.ProjectUtil;
 import com.liferay.sca.util.PropsKeys;
 import com.liferay.sca.util.PropsUtil;
 import com.liferay.sca.util.PropsValues;
 
 import java.io.File;
-import java.io.IOException;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -59,21 +56,6 @@ public class DependencyFinder {
 		_findPackageFiles(srcCodeFile, packageFiles);
 
 		return packageFiles;
-	}
-
-	protected static void save(String project , Set<Dependency> dependencies) 
-		throws IOException {
-
-		StringBuilder sb = new StringBuilder();
-
-		for (Dependency dependency : dependencies) {
-			sb.append(dependency.toString());
-			sb.append("\n");
-		}
-
-		FileUtil.write(
-			sb.toString(),
-			PropsUtil.get(project + "." + PropsKeys.DEPENDENCIES_REPORT));
 	}
 
 	private static void _findPackageFiles(File folder, Set<File> packageFiles) {
