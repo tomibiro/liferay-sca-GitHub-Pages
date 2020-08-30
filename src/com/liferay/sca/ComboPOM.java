07682054fb815f35aaecd5074f34cc9a59f2414a
Samuel Kong <samuel.kong@liferay.com>
2020-08-30 09:36:19 +0800

    #2 Create a combo pom.xml

---
+++ src/com/liferay/sca/ComboPOM.java
@@ -0,0 +1,98 @@
+package com.liferay.sca;
+
+import com.liferay.sca.model.Dependency;
+import com.liferay.sca.model.Report;
+import com.liferay.sca.util.FileUtil;
+import com.liferay.sca.util.ProjectUtil;
+import com.liferay.sca.util.PropsKeys;
+import com.liferay.sca.util.PropsUtil;
+import com.liferay.sca.util.PropsValues;
+
+import java.io.IOException;
+
+import java.util.Set;
+
+public class ComboPOM {
+
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			generate();
+		}
+		else {
+			generate(args[0]);
+		}
+	}
+
+	public static void generate() throws Exception {
+		for (String project : PropsValues.PROJECTS) {
+			generate(project);
+		}
+	}
+
+	public static void generate(String project) throws Exception {
+		ProjectUtil.validate(project);
+
+		StringBuilder sb = new StringBuilder();
+
+		_addHeader(sb, project);
+
+		Report report = Report.load(project);
+
+		Set<Dependency> dependencies = report.getDependencies();
+
+		for (Dependency dependency : dependencies) {
+			_addDependency(sb, dependency);
+		}
+
+		_addFooter(sb);
+
+		save(project, sb.toString());
+	}
+
+	protected static void save(String project, String content) throws IOException {
+		String folder = PropsUtil.get(
+			project + "." + PropsKeys.DEPENDABOT_PACKAGE_DIR);
+
+		String path = folder + "/pom.xml";
+
+		FileUtil.write(content, path);
+	}
+
+	private static void _addDependency(StringBuilder sb, Dependency dependency) {
+		sb.append("\t\t<dependency>\n");
+		sb.append("\t\t\t<groupId>");
+		sb.append(dependency.getGroup());
+		sb.append("</groupId>\n");
+		sb.append("\t\t\t<artifactId>");
+		sb.append(dependency.getArtifact());
+		sb.append("</artifactId>\n");
+		sb.append("\t\t\t<version>");
+		sb.append(dependency.getVersion());
+		sb.append("</version>\n");
+		sb.append("\t\t</dependency>\n");
+	}
+
+	private static void _addFooter(StringBuilder sb) {
+		sb.append("\t</dependencies>\n");
+		sb.append("</project>");
+	}
+
+	private static void _addHeader(StringBuilder sb, String project) {
+		sb.append("<?xml version=\"1.0\"?>\n\n");
+		sb.append("<project\n");
+		sb.append("\txmlns=\"http://maven.apache.org/POM/4.0.0\"\n");
+		sb.append(
+			"\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
+		sb.append(
+			"\txsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 " +
+				"http://maven.apache.org/xsd/maven-4.0.0.xsd\"\n");
+		sb.append(">\n");
+		sb.append("\t<groupId>com.liferay</groupId>\n");
+		sb.append("\t<artifactId>");
+		sb.append(project);
+		sb.append("</artifactId>\n");
+		sb.append("\t<version>0.0.1</version>\n");
+		sb.append("\t<dependencies>\n");
+	}
+
+}
--- src/com/liferay/sca/model/Report.java
+++ src/com/liferay/sca/model/Report.java
@@ -1,6 +1,7 @@
 package com.liferay.sca.model;
 
 import com.liferay.sca.comparator.DependencyComparator;
+import com.liferay.sca.exception.ParseException;
 import com.liferay.sca.exception.ProjectNotConfiguredException;
 import com.liferay.sca.util.FileUtil;
 import com.liferay.sca.util.ProjectUtil;
@@ -8,14 +9,61 @@ import com.liferay.sca.util.PropsKeys;
 import com.liferay.sca.util.PropsUtil;
 
 import java.io.IOException;
+import java.io.File;
 
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Report {
 
+	public static Report load(File file) throws Exception {
+		Report report = new Report();
+
+		String content = FileUtil.read(file);
+
+		String[] lines = content.split("\\n");
+
+		for (String line : lines) {
+			line = line.trim();
+
+			String[] parts = line.split(":");
+
+			if (parts.length != 3) {
+				throw new ParseException(line);
+			}
+
+			Dependency dependency = new Dependency(
+				parts[0], parts[1], parts[2]);
+
+			report.add(dependency);
+		}
+
+		return report;
+	}
+
+	public static Report load(String project) throws Exception {
+		ProjectUtil.validate(project);
+
+		File file = new File(
+			PropsUtil.get(project + "." + PropsKeys.DEPENDENCIES_REPORT));
+
+		Report report = load(file);
+
+		report.setProject(project);
+
+		return report;
+	}
+
+	public void add(Dependency dependency) {
+		_dependencies.add(dependency);
+	}
+
 	public void add(Set<Dependency> dependencies) {
 		_dependencies.addAll(dependencies);
+	}
+
+	public Set<Dependency> getDependencies() {
+		return _dependencies;
 	}
 
 	public void save() throws IOException, ProjectNotConfiguredException {
